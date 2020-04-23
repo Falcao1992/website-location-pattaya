970b8d85857335b9c348d5bf7581133f932714b5
Falcao1992 <eduardo.marselle@gmail.com>
2020-04-23 14:54:18 +0200

    add background image for index component

--- src/components/image.js
+++ src/components/image.js
@@ -1,32 +1,74 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from 'react'
+import {graphql, StaticQuery} from 'gatsby'
+import styled from 'styled-components'
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import BackgroundImage from 'gatsby-background-image-es5'
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
+const BackgroundHomeSection = ({className}) => (
+    <StaticQuery
+        query={graphql`
+        query {
+            welcomePicture: file(relativePath: { eq: "welcomePicture.jpg" }) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1920) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+            apartmentPicture: file(relativePath: { eq: "apartmentPicture.jpg" }) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1920) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+            activityPicture: file(relativePath: { eq: "activityPicture.jpg" }) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1920) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
         }
-      }
-    }
-  `)
+        `}
+        render={data => {
+            const allBackgroundImage = [data.welcomePicture.childImageSharp.fluid, data.apartmentPicture.childImageSharp.fluid, data.activityPicture.childImageSharp.fluid]
+            console.log(allBackgroundImage)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+            const handleChooseBackgroundImageScroll = (img) => {
+                return [`linear-gradient(180deg, rgba(0, 0, 0, 0.5), rgba(28, 28, 28, 0.1))`, img]
+            };
 
-export default Image
+            return (
+                <>
+                    {allBackgroundImage.map((image, index) => {
+                        return (
+                            <BackgroundImage
+                                key={index}
+                                Tag="section"
+                                className={className}
+                                fluid={handleChooseBackgroundImageScroll(image)}
+                                backgroundColor={`#040e18`}
+                            >
+                                <H2Styled>gatsby-background-image</H2Styled>
+                            </BackgroundImage>
+                        )
+                    })}
+                </>
+            )
+        }}
+    />
+)
+const H2Styled = styled.div`
+height: 500px;
+`
+
+const StyledBackgroundHomeSection = styled(BackgroundHomeSection)`
+  background-attachment: fixed;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  overflow: hidden;
+`
+
+export default StyledBackgroundHomeSection
