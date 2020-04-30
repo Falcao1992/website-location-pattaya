3d5ddd90f036151aa5ceaa237b3afc4b0fe896de
Falcao1992 <eduardo.marselle@gmail.com>
2020-04-30 20:59:28 +0200

    refactor homePage background attachement

--- src/components/HomeCategories.js
+++ src/components/HomeCategories.js
@@ -5,7 +5,7 @@ import BackgroundImage from 'gatsby-background-image-es5'
 import "./HomeCategories.css"
 import "typeface-pinyon-script"
 
-import { Controller, Scene } from "react-scrollmagic";
+import {Controller, Scene} from "react-scrollmagic";
 
 const HomeCategories = ({className}) => (
     <StaticQuery
@@ -52,11 +52,36 @@ const HomeCategories = ({className}) => (
         `}
         render={data => {
             const allCategory = [
-                {fluid:data.apartmentPicture.childImageSharp.fluid, backgroundColorDescription:"#6d1441ed", title: "Nos Appartements", page: "apartments"},
-                {fluid:data.activityPicture.childImageSharp.fluid, backgroundColorDescription:"#379cc1", title: "Les Activitées", page: "activity"},
-                {fluid:data.welcomePicture.childImageSharp.fluid, backgroundColorDescription:"#1f5831ed", title: "A Propos", page: "/about"},
-                {fluid:data.interestPicture.childImageSharp.fluid, backgroundColorDescription:"#6d1441ed", title: "les lieux d'interets", page: "/interest"},
-                {fluid:data.contactPicture.childImageSharp.fluid, backgroundColorDescription:"#379cc1", title: "nous contacter", page: "/contact"},
+                {
+                    fluid: data.apartmentPicture.childImageSharp.fluid,
+                    backgroundColorDescription: "#6d1441ed",
+                    title: "Nos Appartements",
+                    page: "apartments"
+                },
+                {
+                    fluid: data.activityPicture.childImageSharp.fluid,
+                    backgroundColorDescription: "#379cc1",
+                    title: "Les Activitées",
+                    page: "activity"
+                },
+                {
+                    fluid: data.welcomePicture.childImageSharp.fluid,
+                    backgroundColorDescription: "#1f5831ed",
+                    title: "A Propos",
+                    page: "/about"
+                },
+                {
+                    fluid: data.interestPicture.childImageSharp.fluid,
+                    backgroundColorDescription: "#6d1441ed",
+                    title: "les lieux d'interets",
+                    page: "/interest"
+                },
+                {
+                    fluid: data.contactPicture.childImageSharp.fluid,
+                    backgroundColorDescription: "#379cc1",
+                    title: "nous contacter",
+                    page: "/contact"
+                },
             ];
 
             const handleChooseBackgroundImageScroll = (img) => {
@@ -68,44 +93,37 @@ const HomeCategories = ({className}) => (
                     {allCategory.map((category, index) => {
                         return (
                             <Controller key={index}>
-                            <Scene
-                                triggerElement={`.box${index}`}
-                                classToggle={[`.box${index}`, "fade-in"]}
-                                offset={-100}
-                                indicators={false}
-                                reverse={false}
-                            >
-                            <BackgroundImage
-                                Tag="section"
-                                className={className}
-                                fluid={handleChooseBackgroundImageScroll(category.fluid)}
-                                backgroundColor={`#040e18`}
-                            >
-                                <DescriptionBlock className={`box${index}`}>
-                                    <h2>{category.title}</h2>
-                                    <p>Note that the development build is not optimized.Note that the development build is not optimizedNote thabuild is not optimizedNote that the development builptimized.</p>
-                                    <SeeMoreLink to={`/${category.page}`}><span>voir plus ></span></SeeMoreLink>
-                                </DescriptionBlock>
-                            </BackgroundImage>
-
-                            </Scene>
+                                <Scene
+                                    triggerElement={`.box${index}`}
+                                    classToggle={[`.box${index}`, "fade-in"]}
+                                    offset={-100}
+                                    indicators={false}
+                                    reverse={false}
+                                >
+                                    <BackgroundImage
+                                        Tag="section"
+                                        className={className}
+                                        fluid={handleChooseBackgroundImageScroll(category.fluid)}
+                                        backgroundColor={`#040e18`}
+                                    >
+                                        <DescriptionBlock className={`box${index}`}>
+                                            <h2>{category.title}</h2>
+                                            <p>Note that the development build is not optimized.Note that the
+                                                development build is not optimizedNote thabuild is not optimizedNote
+                                                that the development builptimized.</p>
+                                            <SeeMoreLink to={`/${category.page}`}><span>voir plus ></span></SeeMoreLink>
+                                        </DescriptionBlock>
+                                    </BackgroundImage>
+                                </Scene>
                             </Controller>
                         )
                     })}
-
                 </div>
             )
         }}
     />
 );
 
-const ContainerBackgroundDescription = styled.section`
-    width: 100%;
-    height: 80vh;
-    display: flex;
-    flex-direction: column;
-    background-color: ${props => props.backgroundColor}
-    `;
 
 const DescriptionBlock = styled.div`
     position: absolute; /* postulat de départ */
@@ -113,8 +131,8 @@ const DescriptionBlock = styled.div`
     font-family: ${props => props.theme.font.primary};
     letter-spacing: 1px;
     text-align: center;
-    background: linear-gradient(90deg,rgba(15,15,18,0.25) 0%,rgba(15, 15, 18, 0.5) 50%,rgba(15,15,18,0.85) 100%);
-    box-shadow: -20px 25px 80px 0 rgba(0,0,0,0.75);
+    background: linear-gradient(90deg,rgba(15,15,18,0.3) 0%,rgba(15, 15, 18, 0.5) 50%,rgba(15,15,18,0.8) 100%);
+    box-shadow: -10px 20px 25px 0 rgba(0,0,0,0.7);
     padding: 1.3rem;
     margin: 0 0.5rem;
     width: calc(100% - 1rem);
@@ -139,14 +157,15 @@ const SeeMoreLink = styled(Link)`
 const StyledCategoryImageParallax = styled(HomeCategories)`
     display: flex;
     align-items: flex-end;
+    width: 100%;
     height: 80vh;
     background-attachment: fixed;
     background-size: cover;
-    background-position: center;
+    background-position: bottom;
     overflow: hidden;
-    padding-bottom: 0.5rem;
-    border-bottom: 4px solid ${props => props.theme.color.secondary}
+    padding-bottom: 1rem;
+    border-bottom: 4px solid ${props => props.theme.color.secondary};
     transition: all 0.6s linear;
-`;
+    `;
 
 export default StyledCategoryImageParallax
